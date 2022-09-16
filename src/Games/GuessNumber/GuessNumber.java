dfe06a39d03705e8277356947183c2119239be10
toborix <matveybor394@gmail.com>
2022-09-17 01:23:57 +0300

    Added a "Guess a number" game

--- src/Games/GuessNumber/GuessNumber.java
+++ src/Games/GuessNumber/GuessNumber.java
@@ -10,21 +10,41 @@ public class GuessNumber {
         System.out.println("The correct guess would be " + computerNumber);
         int count = 1;
 
-        while (userAnswer != computerNumber)
-        {
-            String responce = JOptionPane.showInputDialog(null,
-                    "Enter a guess between 1 and 100", "Guessing Game", 3);
+            while (userAnswer != computerNumber) {
+                String responce = JOptionPane.showInputDialog(null,
+                        "Enter a guess between 1 and 100", "Guessing Game", JOptionPane.QUESTION_MESSAGE);
 
-            userAnswer = Integer.parseInt(responce);
+                userAnswer = Integer.parseInt(responce);
 
-            JOptionPane.showMessageDialog(null,
-                    "" + determineGuess(userAnswer, computerNumber, count));
+                JOptionPane.showMessageDialog(null,
+                        "" + determineGuess(userAnswer, computerNumber, count));
 
-            count++;
-        }
+                count++;
+
+            }
+
+
 
 
     }
 
-    // TODO: написать метод determineGuess
+
+
+    public static String determineGuess(int userAnswer, int computerNumber, int count) {
+
+
+        if(userAnswer <= 0 || userAnswer > 100 ){
+            return "Your guess is invalid";
+        }
+            else if(userAnswer == computerNumber){
+                return "Correct!\nTotal Guesses: " + count;
+            }
+                else if(userAnswer > computerNumber) {
+                    return "Your guess is too high, try lower.\nTry Number: " + count;
+                }
+                    else {
+                        return "Your guess is too low, try higher .\nTry Number: " + count;
+                    }
+
+    }
 }
